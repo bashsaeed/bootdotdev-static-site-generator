07ef0c206853e99dc8a96e79a281882bd33fc131
Bashar Saeed <bwasaeed@gmail.com>
2025-03-24 02:07:05 +0400

    create tests for parentnode

--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,6 +1,6 @@
 import unittest
 
-from htmlnode import HTMLNode, LeafNode
+from htmlnode import HTMLNode, LeafNode, ParentNode
 
 
 class TestHTMLNode(unittest.TestCase):
@@ -61,6 +61,47 @@ class TestLeafNode(unittest.TestCase):
             repr(node),
             "LeafNode(tag='p', value='Hello, world!', props=None, children=None)",
         )
+
+
+class TestParentNode(unittest.TestCase):
+    def test_to_html_with_children(self):
+        child_node = LeafNode("span", "child")
+        parent_node = ParentNode("div", [child_node])
+        self.assertEqual(parent_node.to_html(), "<div><span>child</span></div>")
+
+    def test_to_html_with_grandchildren(self):
+        grandchild_node = LeafNode("b", "grandchild")
+        child_node = ParentNode("span", [grandchild_node])
+        parent_node = ParentNode("div", [child_node])
+        self.assertEqual(
+            parent_node.to_html(),
+            "<div><span><b>grandchild</b></span></div>",
+        )
+
+    def test_to_html_with_multiple_children(self):
+        node = ParentNode(
+            "p",
+            [
+                LeafNode("b", "Bold text"),
+                LeafNode(None, "Normal text"),
+                LeafNode("i", "italic text"),
+                LeafNode(None, "Normal text"),
+            ],
+        )
+        self.assertEqual(
+            node.to_html(),
+            "<p><b>Bold text</b>Normal text<i>italic text</i>Normal text</p>",
+        )
+
+    # def test_parentnode_without_tag_raises(self):
+    #     with self.assertRaises(ValueError) as cm:
+    #         ParentNode(None, [LeafNode("span", "text")])
+    #     self.assertEqual(str(cm.exception), "Parent node must have a tag.")
+
+    # def test_parentnode_without_children_raises(self):
+    #     with self.assertRaises(ValueError) as cm:
+    #         ParentNode("div", [])
+    #     self.assertEqual(str(cm.exception), "Parent node must have children.")
 
 
 if __name__ == "__main__":
