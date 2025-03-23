4b6180fe82841a71ab458733aa199e36de8d44d0
Bashar Saeed <bwasaeed@gmail.com>
2025-03-24 03:30:26 +0400

    create split_nodes_image + tests

--- src/inline_markdown.py
+++ src/inline_markdown.py
@@ -35,3 +35,33 @@ def extract_markdown_links(text: str) -> list[tuple[str, str]]:
     re_links = re.compile(r"\[([^\]]+)\]\(([^)]+)\)")
     all_links = re_links.findall(text)
     return [(text, url) for text, url in all_links]
+
+
+def split_nodes_image(old_nodes):
+    new_nodes = []
+    for old_node in old_nodes:
+        if old_node.text_type != TextType.TEXT:
+            new_nodes.append(old_node)
+            continue
+        original_text = old_node.text
+        images = extract_markdown_images(original_text)
+        if len(images) == 0:
+            new_nodes.append(old_node)
+            continue
+        for image in images:
+            sections = original_text.split(f"![{image[0]}]({image[1]})", 1)
+            if len(sections) != 2:
+                raise ValueError("invalid markdown, image section not closed")
+            if sections[0] != "":
+                new_nodes.append(TextNode(sections[0], TextType.TEXT))
+            new_nodes.append(
+                TextNode(
+                    image[0],
+                    TextType.IMAGE,
+                    image[1],
+                )
+            )
+            original_text = sections[1]
+        if original_text != "":
+            new_nodes.append(TextNode(original_text, TextType.TEXT))
+    return new_nodes
