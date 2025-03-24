4554e0382227f749916e1937e151c251ad32f0c6
Bashar Saeed <bwasaeed@gmail.com>
2025-03-25 02:50:48 +0400

    debug

--- src/main.py
+++ src/main.py
@@ -1,17 +1,17 @@
-import sys
 import os
+import sys
 from generate_page import generate_pages_recursive
 from copy_directory import copy_directory
 
-dir_path_static = "./static"
-dir_path_public = "./docs"
-dir_path_content = "./content"
-template_path = "./template.html"
+dir_path_static = "static"
+dir_path_public = "docs"
+dir_path_content = "content"
+template_path = "template.html"
 
 
 def main():
     args: list[str] = sys.argv
-    basepath = args[0] if args else "/"
+    basepath = args[1] if len(args) > 1 else "./"
     copy_directory(dir_path_static, dir_path_public)
     generate_pages_recursive(
         os.path.join(basepath, dir_path_content),
